be6484a1295cab3553c6ee329ed991a649f8c323
John Jabez <John.Jabez@cognizant.com>
2020-04-22 11:20:24 -0500

    Circuit Breaker

--- applications/allocations-server/src/main/java/io/pivotal/pal/tracker/allocations/App.java
+++ applications/allocations-server/src/main/java/io/pivotal/pal/tracker/allocations/App.java
@@ -3,13 +3,18 @@ package io.pivotal.pal.tracker.allocations;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableOAuth2Client;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.web.client.RestOperations;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 import java.util.TimeZone;
-
+@EnableWebSecurity
+@EnableResourceServer
+@EnableOAuth2Client
 @EnableEurekaClient
 @SpringBootApplication
 @ComponentScan({"io.pivotal.pal.tracker.allocations", "io.pivotal.pal.tracker.restsupport"})
@@ -22,8 +27,8 @@ public class App {
 
     @Bean
     ProjectClient projectClient(
-            RestOperations restOperations,
-            @Value("${registration.server.endpoint}") String registrationEndpoint
+        RestOperations restOperations,
+        @Value("${registration.server.endpoint}") String registrationEndpoint
     ) {
         return new ProjectClient(restOperations, registrationEndpoint);
     }
--- integration-test/src/test/java/test/pivotal/pal/tracker/support/ApplicationServer.java
+++ integration-test/src/test/java/test/pivotal/pal/tracker/support/ApplicationServer.java
@@ -23,8 +23,8 @@ public class ApplicationServer {
 
     public void start(Map<String, String> env) throws IOException, InterruptedException {
         ProcessBuilder processBuilder = new ProcessBuilder()
-                .command("java", "-jar", jarPath)
-                .inheritIO();
+            .command("java", "-jar", jarPath)
+            .inheritIO();
 
         processBuilder.environment().put("SERVER_PORT", port);
         env.forEach((key, value) -> processBuilder.environment().put(key, value));
@@ -36,12 +36,12 @@ public class ApplicationServer {
         String dbUrl = "jdbc:mysql://localhost:3306/" + dbName + "?useSSL=false&useTimezone=true&serverTimezone=UTC&useLegacyDatetimeCode=false";
 
         start(envMapBuilder()
-                        .put("SPRING_DATASOURCE_URL", dbUrl)
-//            .put("REGISTRATION_SERVER_ENDPOINT", "http://localhost:8883")
-                        .put("EUREKA_CLIENT_ENABLED", "false")
-                        .put("RIBBON_EUREKA_ENABLED", "false")
-                        .put("REGISTRATION_SERVER_RIBBON_LISTOFSERVERS", "http://localhost:8883")
-                        .build()
+            .put("SPRING_DATASOURCE_URL", dbUrl)
+            .put("EUREKA_CLIENT_ENABLED", "false")
+            .put("RIBBON_EUREKA_ENABLED", "false")
+            .put("REGISTRATION_SERVER_RIBBON_LISTOFSERVERS", "http://localhost:8883")
+            .put("APPLICATION_OAUTH_ENABLED", "false")
+            .build()
         );
     }
 
@@ -56,7 +56,7 @@ public class ApplicationServer {
 
     private static void waitUntilServerIsUp(String port) throws InterruptedException {
         HttpClient httpClient = new HttpClient();
-        int timeout = 500;
+        int timeout = 120;
         Instant start = Instant.now();
         boolean isUp = false;
 
@@ -80,3 +80,4 @@ public class ApplicationServer {
         }
     }
 }
+
