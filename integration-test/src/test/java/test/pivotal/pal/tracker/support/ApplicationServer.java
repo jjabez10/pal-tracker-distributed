cfe0016788435232c6eb2860447924015af53c26
John Jabez <John.Jabez@cognizant.com>
2020-04-21 19:11:52 -0500

    Service Discovery

--- integration-test/src/test/java/test/pivotal/pal/tracker/support/ApplicationServer.java
+++ integration-test/src/test/java/test/pivotal/pal/tracker/support/ApplicationServer.java
@@ -57,7 +57,7 @@ public class ApplicationServer {
 
     private static void waitUntilServerIsUp(String port) throws InterruptedException {
         HttpClient httpClient = new HttpClient();
-        int timeout = 200;
+        int timeout = 500;
         Instant start = Instant.now();
         boolean isUp = false;
 
